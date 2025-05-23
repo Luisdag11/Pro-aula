860b4966c851f6c7bf9694b88cd42d70c8e0cd34
Luis Daniel Garavito Hernandez <luisdagh11@gmail.com>
2025-05-23 16:04:03 -0500

    UsuarioJavaListo

---
+++ Desktop/GestionUsuarios/src/co/edu/Unicolombo/pb/datos/Usuario.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package co.edu.Unicolombo.pb.datos;
+
+import java.util.*;
+
+/**
+ *
+ * @author Luis Garavito
+ */
+public class Usuario {
+    public String nombre;  
+    public String telefono; 
+    public String documento; 
+    public String email;
+    public String codigo;
+    public String carrera;
+    public String semestre; 
+    public String clave; 
+    
+    public static HashMap<String, Usuario> usuariosBD;
+}
